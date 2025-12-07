6a4b07d955cead6d24a4fdaedc303731eacfd81c
Tim Collins <timcollinsdesign+metasignal@gmail.com>
2025-12-06 19:38:27 -0500

    first steps towards becoming a fastapi wizard

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    return {
+            "book_id" : book_id,
+            "title" : "The Great Gatsby",
+            "author": "F Scott Fitzgerald"
+
+    }
+
+@app.get("/authors/{author_id}")
+async def read_author(author_id: int):
+    return {
+        "author_id": author_id,
+        "name": "Ernest Hemingway"
+    }
+
+@app.get("/books")
+async def read_books(year: int = None):
+    if year:
+        return {
+                "year": year,
+                "books": ["Book 1", "Book 2"]
+        }
+    return {"books": ["All Books"]}
