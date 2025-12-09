4ff2783174c88a5048ffca49292359eab1aa1c5b
Tim Collins <timcollinsdesign+metasignal@gmail.com>
2025-12-08 22:17:12 -0500

    updated error handling and added new bruno tests

--- items/router.py
+++ items/router.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/items", tags=["items"])
 
-@router.get("/items/{item_id}")
+@router.get("/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
